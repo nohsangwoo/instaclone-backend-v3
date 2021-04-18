1a1deeddd59f2dad7e8d2b76a3d953d600e92aea
nohsangwoo <nsgr12@naver.com>
2021-04-18 16:44:17 +0900

    #6.4 Upload Photo part Three

--- photos/uploadPhoto/uploadPhoto.resolvers.js
+++ photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -5,29 +5,30 @@ export default {
   Mutation: {
     uploadPhoto: protectedResolver(
       async (_, { file, caption }, { loggedInUser }) => {
+        let hashtagObj = [];
         if (caption) {
           // 정규 표현식(match로 정규표현식에 해당하는 단어를 모두 추출하여 배열로 반환한다)
           // #으로 시작하는 모든 단어
           const hashtags = caption.match(/#[\w]+/g);
+          hashtagObj = hashtags.map((hashtag) => ({
+            where: { hashtag },
+            create: { hashtag },
+          }));
         }
-        client.photo.create({
+        return client.photo.create({
           data: {
             file,
             caption,
-            hashtags: {
-              // 입력받은 hashtag로 검색하서 존재하면 연결하고
-              // 없으면 새로 만든 후 연결한다
-              connectOrCreate: [
-                {
-                  where: {
-                    hashtag: '#food',
-                  },
-                  create: {
-                    hashtag: '#food',
-                  },
-                },
-              ],
+            user: {
+              connect: {
+                id: loggedInUser.id,
+              },
             },
+            ...(hashtagObj.length > 0 && {
+              hashtags: {
+                connectOrCreate: hashtagObj,
+              },
+            }),
           },
         });
       }
