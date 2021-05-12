ddb0d9ecd5d8bfbd1046be0de1b10a3ad29a8847
nohsangwoo <nsgr12@naver.com>
2021-05-12 15:19:19 +0900

    #7.5 readMessage add comments

--- messages/messages.resolvers.js
+++ messages/messages.resolvers.js
@@ -13,12 +13,14 @@ export default {
       if (!loggedInUser) {
         return 0;
       }
+      //   message의 read:false이고 roomId:parentId 그리고, user의 id는 로그인한 유저의 id만 아니면됨
       return client.message.count({
         where: {
           read: false,
           roomId: id,
           user: {
             id: {
+              //id: loggedInUser.id가 아닌 조건
               not: loggedInUser.id,
             },
           },
--- messages/readMessage/readMessage.resolvers.js
+++ messages/readMessage/readMessage.resolvers.js
@@ -6,10 +6,13 @@ export default {
     readMessage: protectedResolver(async (_, { id }, { loggedInUser }) => {
       const message = await client.message.findFirst({
         where: {
+          // 전달받은 메시지 Id로 정말 메시지가 존재하는지 확인
           id,
+          // 내가 그 메시지를 보낸 사용자가 아닐때
           userId: {
             not: loggedInUser.id,
           },
+          //   내가 해당 대확방에 들어가져있는 상태인지 확인
           room: {
             users: {
               some: {
@@ -18,20 +21,28 @@ export default {
             },
           },
         },
+        // 위 조건을 통과하면 메시지를 수정할수있는 상태를 알리는 조건으로 메시지 id를 반환
         select: {
           id: true,
         },
       });
+
+      // 메시지를 찾지 못했다면 에러 핸들링
       if (!message) {
         return {
           ok: false,
           error: 'Message not found.',
         };
       }
+
+      // 메시지를 찾았다면 그다음
+      // 메시지를 업데이트 해준다
       await client.message.update({
+        // 전달받은 메시지 id에 업데이트하고
         where: {
           id,
         },
+        // 수정할 field는 read이고 수정할 값은 false=> true
         data: {
           read: true,
         },
